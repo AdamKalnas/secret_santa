b24f46cd7a6fdcee7a95689ad4e139f65328a9b5
Adam Kalnas <a.kalnas@modcloth.com>
2013-12-20 11:38:35 -0500

    When there is only one participant in secret santa, it's very lonely
    Signed-off-by: Rachel Thompson <r.thompson@modcloth.com>

--- spec/secret_santa_test.go
+++ spec/secret_santa_test.go
@@ -1,15 +1,21 @@
 package spec_test
 
 import (
-	//_ "github.com/adamkalnas/secret_santa/spec"
 	"github.com/adamkalnas/secret_santa/secret_santa"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("SecretSanta", func() {
-	It("Should not e-mail anyone when no names are provided", func() {
+	It("Should not match anyone when no names are provided", func() {
 		list_of_participants := []string{}
+		matches := []string{}
+
+		Expect(secret_santa.GetMatches(list_of_participants)).To(Equal(matches))
+	})
+
+	It("Should not match anyone when there is only one participant", func() {
+		list_of_participants := []string{"Renea"}
 		matches := []string{}
 
 		Expect(secret_santa.GetMatches(list_of_participants)).To(Equal(matches))
