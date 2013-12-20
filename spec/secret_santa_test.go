d4d3a18fb6e665d13733c903233e0a670510a9f9
Rachel Thompson <r.thompson@modcloth.com>
2013-12-20 11:59:46 -0500

    Matches two people to each other.
    Signed-off-by: Adam Kalnas <a.kalnas@modcloth.com>

--- spec/secret_santa_test.go
+++ spec/secret_santa_test.go
@@ -9,14 +9,23 @@ import (
 var _ = Describe("SecretSanta", func() {
 	It("Should not match anyone when no names are provided", func() {
 		list_of_participants := []string{}
-		matches := []string{}
+		matches := make(map[string]string)
 
 		Expect(secret_santa.GetMatches(list_of_participants)).To(Equal(matches))
 	})
 
 	It("Should not match anyone when there is only one participant", func() {
 		list_of_participants := []string{"Renea"}
-		matches := []string{}
+		matches := make(map[string]string)
+
+		Expect(secret_santa.GetMatches(list_of_participants)).To(Equal(matches))
+	})
+
+	It("Should match two people to eachother when there are only two participants", func() {
+		list_of_participants := []string{"Renea", "Rachel"}
+		matches := make(map[string]string)
+		matches["Renea"] = "Rachel"
+		matches["Rachel"] = "Renea"
 
 		Expect(secret_santa.GetMatches(list_of_participants)).To(Equal(matches))
 	})
