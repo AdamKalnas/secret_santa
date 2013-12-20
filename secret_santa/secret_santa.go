2ac3a9c448ed0adaee74196af48c95b9f7af0801
Adam Kalnas <a.kalnas@modcloth.com>
2013-12-20 15:32:37 -0500

    Refactored the code to make it more readable.
    Signed-off-by: Rachel Thompson <r.thompson@modcloth.com>

--- secret_santa/secret_santa.go
+++ secret_santa/secret_santa.go
@@ -1,5 +1,51 @@
 package secret_santa
 
-func GetMatches(participants []string) ([]string) {
-  return []string{}
+import (
+	"math/rand"
+	"time"
+)
+
+func GetMatches(participants []string) map[string]string {
+	matches := make(map[string]string)
+	if len(participants) > 1 {
+		return repeatUntilValid(participants)
+	}
+	return matches
+}
+
+func repeatUntilValid(originals []string) (matches map[string]string) {
+	for {
+		matches = obtainMatches(originals)
+		if !containsReflexiveMatches(matches) {
+			return matches
+		}
+	}
+}
+
+func containsReflexiveMatches(matches map[string]string) bool {
+	for key, value := range matches {
+		if key == value {
+			return true
+		}
+	}
+	return false
+}
+
+func obtainMatches(originals []string) map[string]string {
+	matches := make(map[string]string)
+	shuffled := shuffle(originals)
+	for i, original := range originals {
+		matches[original] = shuffled[i]
+	}
+	return matches
+}
+
+func shuffle(original []string) []string {
+	shuffled := make([]string, len(original))
+	rand.Seed(time.Now().UTC().UnixNano())
+	perm := rand.Perm(len(original))
+	for i, v := range perm {
+		shuffled[v] = original[i]
+	}
+	return shuffled
 }
